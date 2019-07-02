4fd7011339c6ff149c4cedad3fc10719d44b4e9f
Ubuntu <ubuntu@ip-172-31-30-122.eu-west-1.compute.internal>
2019-07-02 14:31:28 +0000

    odified

--- accounts/views.py
+++ accounts/views.py
@@ -62,7 +62,6 @@ def registration(request):
     return render(request, 'registration.html', {
         "registration_form": registration_form})
 
-
 def user_profile(request):
     """The user's profile page"""
     user = User.objects.get(email=request.user.email)
--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -8,7 +8,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-# import env
+import env
 import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -19,12 +19,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in ticketion secret!
-SECRET_KEY = '6rta5@qrc4v7@%nuizck-#rasnm46qjlkabl9bwf1%!tt@tu21'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in ticketion!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get("C9_HOSTNAME"), '127.0.0.1']
+ALLOWED_HOSTS = ['7b802ac4fabd408db01336ff783f2d87.vfs.cloud9.eu-west-1.amazonaws.com']
 
 
 # Application definition
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'sorting',
     'home',
     'tickets',
-    'payments',
+    'payment',
+    'cart'
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+            
             ],
         },
     },
@@ -81,12 +83,14 @@ WSGI_APPLICATION = 'issue_tracker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
+DATABASES = {'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
