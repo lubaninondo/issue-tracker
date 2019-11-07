531ad6af471e4c962d6751a0ea7e8495976726f3
Ubuntu <ubuntu@ip-172-31-30-122.eu-west-1.compute.internal>
2019-11-07 23:00:23 +0000

    app modifications

--- comments/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import url, include
-from .views import create_comment
-
-urlpatterns = [
-    url(r'^comment/(?P<pk>\d+)$', create_comment, name='create_comment'),
-]
--- issue_tracker/settings.py
+++ issue_tracker/settings.py
@@ -42,7 +42,6 @@ INSTALLED_APPS = [
     'accounts',
     'tickets',
     'blog',
-    'comments',
     'statistic',
     
   
