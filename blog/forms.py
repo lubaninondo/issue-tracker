e2eb0f396ec2e184fd8f0a9c3edf8f57ea1b1e80
Ubuntu <ubuntu@ip-172-31-30-122.eu-west-1.compute.internal>
2019-12-28 19:27:23 +0000

    Blog and Base modified

--- blog/forms.py
+++ blog/forms.py
@@ -4,4 +4,4 @@ from .models import Post
 class BlogPostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'content', 'image', 'tag')
+        fields = ('title', 'content')
